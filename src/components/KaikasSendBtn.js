b096b33222143ab7ce5170c28ccb7bac69c99d70
Namhoon Lee <frontalnh@gmail.com>
2022-04-11 21:10:39 +0900

    update

--- src/components/KaikasSendBtn.js
+++ src/components/KaikasSendBtn.js
@@ -1,92 +1,87 @@
-import Caver from 'caver-js'
-import React from 'react'
-import { connect } from 'react-redux'
-import { Button } from "react-bootstrap"
-import {
-  setKaikasAddressRoutine,
-  kaikasSendTxRoutine,
-} from '../modules/kaikas'
+import Caver from "caver-js";
+import React from "react";
+import { connect } from "react-redux";
+import { Button } from "react-bootstrap";
+import { setKaikasAddressRoutine, kaikasSendTxRoutine } from "../modules/kaikas";
 
 const KaikasSendBtn = (props) => {
-  const [kaikasAddress, setKaikasAddress] = React.useState(localStorage.getItem('kaikasAddress'))
-  React.useEffect(()=>{
-    const value = props.kaikasReducer[props.setKaikasAddressRoutine.SUCCESS]
-    if(value) {
-      if(value.klaytnAddress) {
-        setKaikasAddress(value.klaytnAddress)
+  const [kaikasAddress, setKaikasAddress] = React.useState(localStorage.getItem("kaikasAddress"));
+  React.useEffect(() => {
+    const value = props.kaikasReducer[props.setKaikasAddressRoutine.SUCCESS];
+    if (value) {
+      if (value.klaytnAddress) {
+        setKaikasAddress(value.klaytnAddress);
       } else {
-        setKaikasAddress('')
+        setKaikasAddress("");
       }
-    } 
-  },[props.kaikasReducer[props.setKaikasAddressRoutine.SUCCESS]])
-
-  React.useEffect(()=>{
-    const value = props.kaikasReducer[props.kaikasSendTxRoutine.SUCCESS]
-    if(value) {
-      window.location.href='/home'
     }
-  },[props.kaikasReducer[props.kaikasSendTxRoutine.SUCCESS]])
+  }, [props.kaikasReducer[props.setKaikasAddressRoutine.SUCCESS]]);
+
+  React.useEffect(() => {
+    const value = props.kaikasReducer[props.kaikasSendTxRoutine.SUCCESS];
+    if (value) {
+      window.location.href = "/home";
+    }
+  }, [props.kaikasReducer[props.kaikasSendTxRoutine.SUCCESS]]);
 
   const sendKaikas = async () => {
-    const caver = new Caver(window.klaytn)
-    const from = props.from
-    const to = props.to
-    const tokenId = props.tokenId
-    const contractAddress = props.contractAddress
+    const caver = new Caver(window.klaytn);
+    const from = props.from;
+    const to = props.to;
+    const tokenId = props.tokenId;
+    const contractAddress = props.contractAddress;
 
     const data = caver.klay.abi.encodeFunctionCall(
       {
-        "constant": false,
-        "inputs": [
+        constant: false,
+        inputs: [
           {
-            "name": "from",
-            "type": "address"
+            name: "from",
+            type: "address",
           },
           {
-            "name": "to",
-            "type": "address"
+            name: "to",
+            type: "address",
           },
           {
-            "name": "tokenId",
-            "type": "uint256"
-          }
+            name: "tokenId",
+            type: "uint256",
+          },
         ],
-        "name": "transferFrom",
-        "outputs": [],
-        "payable": false,
-        "stateMutability": "nonpayable",
-        "type": "function"
+        name: "transferFrom",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function",
       },
-      [
-        from,
-        to,
-        caver.utils
-          .toBN(tokenId)
-          .toString()
-      ]
-    )
+      [from, to, caver.utils.toBN(tokenId).toString()]
+    );
     const txData = {
-      type: 'FEE_DELEGATED_SMART_CONTRACT_EXECUTION',
+      type: "FEE_DELEGATED_SMART_CONTRACT_EXECUTION",
       from,
       to: contractAddress,
       gas: 1000000,
-      data
-    }
+      data,
+    };
 
-    const signed = await caver.klay.signTransaction(txData)
-    props.kaikasSendTxRoutineDispatcher({rawTransaction:signed.rawTransaction})
-  }
+    const signed = await caver.klay.signTransaction(txData);
+    props.kaikasSendTxRoutineDispatcher({ rawTransaction: signed.rawTransaction });
+  };
 
   return (
-    <Button disabled={props.disabled} onClick={sendKaikas}>Kaikas로 보내기</Button>
-  )
+    <Button disabled={props.disabled} onClick={sendKaikas}>
+      Kaikas로 보내기
+    </Button>
+  );
+};
 
-}
-
-export default connect((state)=>({
-  ...state,
-  setKaikasAddressRoutine,
-  kaikasSendTxRoutine,
-}),{
-  kaikasSendTxRoutineDispatcher:kaikasSendTxRoutine,
-})(KaikasSendBtn)
+export default connect(
+  (state) => ({
+    ...state,
+    setKaikasAddressRoutine,
+    kaikasSendTxRoutine,
+  }),
+  {
+    kaikasSendTxRoutineDispatcher: kaikasSendTxRoutine,
+  }
+)(KaikasSendBtn);
--- src/components/NFTGroup.js
+++ src/components/NFTGroup.js
@@ -1,46 +1,43 @@
-import React from "react"
-import { CardDeck } from "react-bootstrap"
-import NFTCard from "./NFTCard"
+import React from "react";
+import { CardDeck } from "react-bootstrap";
+import NFTCard from "./NFTCard";
 
 export const NFTGroup = (props) => {
-  const width = 4
-  const [items, setItems] = React.useState([])
+  const width = 4;
+  const [items, setItems] = React.useState([]);
 
-  React.useEffect(()=>{
-    var arr = []
-    var col = 0
-    var row = 0
-    props.nfts.forEach((e,i) => {
-      col = i % width
-      row = Math.floor(i / width)
+  React.useEffect(() => {
+    var arr = [];
+    var col = 0;
+    var row = 0;
+    props.nfts.forEach((e, i) => {
+      col = i % width;
+      row = Math.floor(i / width);
 
-      if( col === 0 ) {
-        arr.push([])
+      if (col === 0) {
+        arr.push([]);
       }
-      arr[row].push(e)
-    })
-    if( props.nfts.length > 0 ) {
-      col += 1
-      if(col > 0) {
-        for(var i = 0; i < width-col;i++) {
-          arr[row].push({id:''})
+      arr[row].push(e);
+    });
+    if (props.nfts.length > 0) {
+      col += 1;
+      if (col > 0) {
+        for (var i = 0; i < width - col; i++) {
+          arr[row].push({ id: "" });
         }
       }
     }
 
-    setItems(arr)
-  },[props.nfts])
+    setItems(arr);
+  }, [props.nfts]);
 
-
-  return (
-    items.map((row,rowIdx) => {
-      return <CardDeck key={`card-deck-row-${rowIdx}`} className='my-4'>
-        {row.map((col,colIdx)=> {
-          return <NFTCard key={`card-deck-row-${rowIdx}-col-${colIdx}`} nft={col} />
-        })
-      }
+  return items.map((row, rowIdx) => {
+    return (
+      <CardDeck key={`card-deck-row-${rowIdx}`} className="my-4">
+        {row.map((col, colIdx) => {
+          return <NFTCard key={`card-deck-row-${rowIdx}-col-${colIdx}`} nft={col} />;
+        })}
       </CardDeck>
-    })
-  )
-
-}
+    );
+  });
+};
